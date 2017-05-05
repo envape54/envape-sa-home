1cec895016163b10e24d60ad68f38015cdada200
Hannes Swanepoel <swanepoelh@hq.za.ovationsgroup.com>
2017-05-05 10:35:57 +0200

    Social component

---
+++ src/app/social/social.module.ts
@@ -0,0 +1,27 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
+
+import { SocialComponent } from "./social.component";
+
+const SocialRoute: Routes = [
+	{
+		path: "social",
+		component: SocialComponent
+	}
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+	RouterModule.forRoot(SocialRoute)
+  ],
+  declarations: [
+	  SocialComponent
+  ],
+  bootstrap: [
+	  SocialComponent
+  ]
+})
+export class SocialModule { }
