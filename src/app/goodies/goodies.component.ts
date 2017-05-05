64e06e88083ecac06d216c3330aaad71e84e4c84
Hannes Swanepoel <swanepoelh@hq.za.ovationsgroup.com>
2017-05-05 12:51:03 +0200

    Components basicafied

--- src/app/goodies/goodies.component.ts
+++ src/app/goodies/goodies.component.ts
@@ -2,15 +2,15 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "envp-goodies",
-  templateUrl: "./goodies.component.html",
-  styleUrls: ["./goodies.component.scss"]
+	selector: "envp-goodies",
+	templateUrl: "./goodies.component.html",
+	styleUrls: ["./goodies.component.scss"]
 })
 export class GoodiesComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
 
+	}
 }
